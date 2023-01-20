09d2e1a41a9a766662f15b78bf5af6bd5e23814c
BK2004 <thunder10.bk@gmail.com>
2023-01-19 18:24:27 -0600

    Finished post template

--- lib/post.dart
+++ lib/post.dart
@@ -1,31 +1,27 @@
 import 'package:flutter/material.dart';
-import 'dart:ui';
+import 'package:flutter/rendering.dart';
 
 class Post extends StatefulWidget {
   final String badge;
   final String username;
   final int time;
-  final String content;
-  final List<FilterInfo> filters;
+  final String imageUrl;
+  final String caption;
+  final List<String> filters;
   final LikeInfo likeInfo;
 
   const Post({Key? key,
     required this.badge,
     required this.username,
     required this.time,
-    required this.content,
+    required this.imageUrl,
+    required this.caption,
     required this.filters,
     required this.likeInfo,
   }) : super(key: key);
 
   @override
   State<Post> createState() => _PostState();
-}
-class FilterInfo {
-  String label;
-  bool isSelected = false;
-
-  FilterInfo(this.label);
 }
 
 class LikeInfo {
@@ -39,19 +35,20 @@ class _PostState extends State<Post> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: [BoxShadow(spreadRadius: 1.0, blurRadius: 2.0, color: Colors.black12)],
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          postLeft(context),
-          postRight(context),
-        ],
-      )
+        padding: const EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [const BoxShadow(spreadRadius: 1.0, blurRadius: 2.0, color: Colors.black12)],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            postLeft(context),
+            postRight(context),
+          ],
+        )
     );
   }
 
@@ -59,12 +56,12 @@ class _PostState extends State<Post> {
     return Column(
       children: [
         Container(
-          margin: const EdgeInsets.all(8.0),
-          child: CircleAvatar(
-            radius: 25.0,
-            backgroundColor: Theme.of(context).colorScheme.primary,
-            // TODO: Load badge based off of id given
-          )
+            margin: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              radius: 25.0,
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              // TODO: Load badge based off of id given
+            )
         ),
         IconButton(
           icon: widget.likeInfo.isLiked ? Icon(Icons.favorite, color: Theme.of(context).colorScheme.primary) : Icon(Icons.favorite_border, color: Theme.of(context).colorScheme.primary),
@@ -111,11 +108,54 @@ class _PostState extends State<Post> {
               )
             ],
           ),
+          Container(
+            margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 15.0, bottom: 15.0),
+            decoration: BoxDecoration(
+              border: Border.all(width: 1.0, color: Colors.black12),
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: const [BoxShadow(spreadRadius: 1.0, blurRadius: 2.0, color: Colors.black12)],
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Image.network(
+                widget.imageUrl,
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
           Text(
-            widget.content,
+            widget.caption,
             overflow: TextOverflow.clip,
             style: Theme.of(context).textTheme.bodyMedium,
-          )
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10.0),
+            constraints: const BoxConstraints(maxHeight: 30.0),
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: widget.filters.length,
+              separatorBuilder: (BuildContext context, int index) => const VerticalDivider(thickness: 0, width: 10.0),
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: const EdgeInsets.only(top: 2.0, bottom: 2.0, left: 30.0, right: 30.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  child: Center(
+                    child: Text(
+                        widget.filters[index],
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        )
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
