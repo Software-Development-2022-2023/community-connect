a54658750b36ff4831c222c41fef0e5f60b26f9a
randomcoder101 <92399448+randomcoder101@users.noreply.github.com>
2023-01-24 00:33:34 -0600

    Update leaderboard.dart

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -1,3 +1,242 @@
 import 'package:flutter/material.dart';
 
 // TODO: Create a class named LeaderboardScreen or something.
+
+class LeaderboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Color.fromARGB(255, 13, 168, 46)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+
+        ///appBar: AppBar(
+        ///backgroundColor: Colors.transparent,
+        ///elevation: 0.0,
+        ///leading: Icon(Icons.arrow_back_ios, color:Colors.green),
+        ///actions: [Icon(Icons.grid_view, color: Colors.green,),],
+        ///),
+        body: Column(
+          children: [
+            Container(
+              height: 55,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15), color: Colors.grey),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          'Name',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 14),
+                        ),
+                        SizedBox(
+                          width: 65,
+                        ),
+                        Text(
+                          'Rank',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 14),
+                        ),
+                        SizedBox(
+                          width: 65,
+                        ),
+                        Text(
+                          'Followers',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 14),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 35,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(children: [
+                      SizedBox(
+                        width: 65,
+                      ),
+                      Text(
+                        'You',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14),
+                      ),
+                      SizedBox(
+                        width: 90,
+                      ),
+                      Text(
+                        '1',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14),
+                      ),
+                      SizedBox(
+                        width: 90,
+                      ),
+                      Text(
+                        '10000',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14),
+                      ),
+                    ]),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 35,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(children: [
+                      SizedBox(
+                        width: 65,
+                      ),
+                      Text(
+                        'Player 1',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14),
+                      ),
+                      SizedBox(
+                        width: 65,
+                      ),
+                      Text(
+                        '2',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14),
+                      ),
+                      SizedBox(
+                        width: 90,
+                      ),
+                      Text(
+                        '9865',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14),
+                      ),
+                    ]),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 35,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(children: [
+                      SizedBox(
+                        width: 65,
+                      ),
+                      Text(
+                        'Player 2',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14),
+                      ),
+                      SizedBox(
+                        width: 65,
+                      ),
+                      Text(
+                        '3',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14),
+                      ),
+                      SizedBox(
+                        width: 90,
+                      ),
+                      Text(
+                        '9122',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14),
+                      ),
+                    ]),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 35,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(children: [
+                      SizedBox(
+                        width: 65,
+                      ),
+                      Text(
+                        'Player 3',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14),
+                      ),
+                      SizedBox(
+                        width: 65,
+                      ),
+                      Text(
+                        '4',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14),
+                      ),
+                      SizedBox(
+                        width: 90,
+                      ),
+                      Text(
+                        '8613',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14),
+                      ),
+                    ]),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
